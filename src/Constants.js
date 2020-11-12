ddd724bb1fb6073e3ae0f9d02bfef303f87c5170
Per Gravdal <per.gravdal@hotmail.com>
2020-11-12 14:26:24 +0100

    change contstans to heroku

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8080'
+export const API_URL = 'http://https://todoapp-scano.herokuapp.com'
 
 export const JPA_API_URL = 'https://todoapp-scano.herokuapp.com/jpa'
 
